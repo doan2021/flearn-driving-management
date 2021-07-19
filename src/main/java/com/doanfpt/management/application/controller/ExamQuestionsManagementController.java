f3c6308bedaaccd1b4529c39baf3e374251a78e2
tamcoding <winsociu1999@gmail.com>
2021-07-19 15:15:32 +0700

    update source

--- src/main/java/com/doanfpt/management/application/controller/ExamQuestionsManagementController.java
+++ src/main/java/com/doanfpt/management/application/controller/ExamQuestionsManagementController.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.doanfpt.management.application.dto.ExamForm;
+
+import com.doanfpt.management.application.common.Constant;
+import com.doanfpt.management.application.dto.ExamQuestionsForm;
 import com.doanfpt.management.application.services.ExamServices;
+import com.doanfpt.management.application.services.QuestionServices;
 
 @Controller
 @RequestMapping("/management")
@@ -16,14 +17,20 @@ public class ExamQuestionsManagementController {
 
 	@Autowired
 	ExamServices examServices;
+	
+	@Autowired
+	QuestionServices questionServices;
 
 	@GetMapping(value = { "/exam-questions-management" })
-	public String examQuestionManagement(Model model) {
+	public String examQuestionManagement(Integer pageNumber, Model model) {
 		return "exam-questions-management";
 	}
 	
 	@GetMapping(value = { "/create-exam-questions" })
-	public String createExamQuestionManagement(Model model) {
+	public String createExamQuestionManagement(Integer pageNumber, Model model) {
+		ExamQuestionsForm examQuestionsForm = new ExamQuestionsForm();
+		model.addAttribute("examQuestionsForm", examQuestionsForm);
+		model.addAttribute(Constant.PAGE_CONTENT_NAME, questionServices.getAllQuestions(pageNumber));
 		return "create-exam-questions";
 	}
 
