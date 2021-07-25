95890e7e8b50499b2c1a3c817bd37712d6563f9d
tamcoding <winsociu1999@gmail.com>
2021-07-25 17:21:15 +0700

    update exam-questions;  add file ExamQuestionsManagementController, FormSearchExamQuestions; ExamQuestionsRepo; ExamQuestionService; ExamQuestionsSpecification and exam-question-management

--- src/main/java/com/doanfpt/management/application/controller/ExamQuestionsManagementController.java
+++ src/main/java/com/doanfpt/management/application/controller/ExamQuestionsManagementController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.doanfpt.management.application.common.Constant;
 import com.doanfpt.management.application.dto.ExamQuestionsForm;
+import com.doanfpt.management.application.dto.FormSearchExamQuestions;
+import com.doanfpt.management.application.services.ExamQuestionsServices;
 import com.doanfpt.management.application.services.ExamServices;
 import com.doanfpt.management.application.services.QuestionServices;
 
@@ -20,11 +23,9 @@ public class ExamQuestionsManagementController {
 	
 	@Autowired
 	QuestionServices questionServices;
-
-	@GetMapping(value = { "/exam-questions-management" })
-	public String examQuestionManagement(Integer pageNumber, Model model) {
-		return "exam-questions-management";
-	}
+	
+	@Autowired
+	ExamQuestionsServices examQuestionsServices;
 	
 	@GetMapping(value = { "/create-exam-questions" })
 	public String createExamQuestionManagement(Integer pageNumber, Model model) {
@@ -33,5 +34,19 @@ public class ExamQuestionsManagementController {
 		model.addAttribute(Constant.PAGE_CONTENT_NAME, questionServices.getAllQuestions(pageNumber));
 		return "create-exam-questions";
 	}
+	
+    @GetMapping(value = { "/exam-questions" })
+    public String visitExamQuestionsForm(FormSearchExamQuestions formSearchExamQuestions, Model model) {
+        model.addAttribute(Constant.PAGE_CONTENT_NAME, examQuestionsServices.searchExamQuestions(formSearchExamQuestions));
+        model.addAttribute("formSearchExamQuestions", new FormSearchExamQuestions());
+        return "exam-questions-management";
+    }
+    
+    @PostMapping(value = { "/search-exam-questions" })
+    public String searchExamQuestions(FormSearchExamQuestions formSearchExamQuestions, Model model) {
+        model.addAttribute(Constant.PAGE_CONTENT_NAME, examQuestionsServices.searchExamQuestions(formSearchExamQuestions));
+        model.addAttribute("formSearchExamQuestions", formSearchExamQuestions);
+        return "exam-questions-management";
+    }
 
 }
---
+++ src/main/java/com/doanfpt/management/application/responsitories/ExamQuestionsRepository.java
@@ -0,0 +1,14 @@
+package com.doanfpt.management.application.responsitories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.doanfpt.management.application.entities.Chapter;
+import com.doanfpt.management.application.entities.ExamQuestions;
+
+@Repository
+public interface ExamQuestionsRepository extends JpaRepository<ExamQuestions, Long>, PagingAndSortingRepository<ExamQuestions, Long>, JpaSpecificationExecutor<ExamQuestions> {
+	 public Chapter findByExamQuestionsIdAndIsDelete(Long examQuestionId, boolean isDelete);
+}
